2dad9ba1f8327db4de7c02bf019a7371e3251fbc
Erick 光 <erickcestari03@gmail.com>
2024-10-18 15:19:56 -0300

    feat: increase description length

--- src/features/product/dao/entity/Product.kt
+++ src/features/product/dao/entity/Product.kt
@@ -6,7 +6,7 @@ object Product : Table("product") {
     val id = long("id").autoIncrement().uniqueIndex()
     val name = varchar("name", 255)
     val price = decimal("price", 9,2)
-    val description = varchar("description", 555)
+    val description = varchar("description", 5555)
     val image = text("image").nullable()
     val isDeleted = bool("isDeleted").default(false)
 
