a1d0b46a5e852d28b8f7155db18374dd8838cd16
Erick 光 <erickcestari03@gmail.com>
2024-10-18 09:59:19 -0300

    feat: update image db type

--- src/features/product/dao/entity/Product.kt
+++ src/features/product/dao/entity/Product.kt
@@ -7,7 +7,7 @@ object Product : Table("product") {
     val name = varchar("name", 255)
     val price = long("price")
     val description = varchar("description", 555)
-    val image = varchar("image", 255).nullable()
+    val image = text("image").nullable()
     val isDeleted = bool("isDeleted").default(false)
 
     override val primaryKey = PrimaryKey(id)
