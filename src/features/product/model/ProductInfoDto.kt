6eb39a1486a230d049397ba8174bdebe18f53b2e
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-18 15:13:23 -0300

    fix: missing BigDecimal

--- src/features/product/model/ProductInfoDto.kt
+++ src/features/product/model/ProductInfoDto.kt
@@ -1,9 +1,11 @@
 package com.blockotlin.features.product.model
 
+import java.math.BigDecimal
+
 data class ProductInfoDto(
     val id: Long,
     val name: String,
-    val price: Long,
+    val price: BigDecimal,
     val image: String?,
     val description: String,
 )
