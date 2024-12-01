412f13d2d538e3fc27d1ccb9b0ac4bcd4784abe5
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-12-01 12:11:10 -0300

    fix: cart routes not checking token from cookie or auth header

--- src/features/cart/routes/getUserCart/GetUserCart.kt
+++ src/features/cart/routes/getUserCart/GetUserCart.kt
@@ -1,5 +1,7 @@
 package com.blockotlin.features.cart.routes.getUserCart
 
+import com.blockotlin.extensions.getTokenFromAuthorization
+import com.blockotlin.extensions.getTokenFromCookie
 import com.blockotlin.features.cart.data.CartData
 import com.blockotlin.jwt.JwtManager
 import io.ktor.http.*
@@ -13,10 +15,10 @@ fun Route.getUserCart() {
     val cartData: CartData by inject(CartData::class.java)
 
     get("/api/v1/cart/user") {
-        val authHeader = call.request.header(HttpHeaders.Authorization)
-            ?: return@get call.respond(HttpStatusCode.Unauthorized, "Missing Authorization Header")
-
-        val token = authHeader.removePrefix("Bearer ").trim()
+        var token = call.getTokenFromCookie()
+        if(token == null) {
+            token = call.getTokenFromAuthorization()!!
+        }
 
         val jwtManager: JwtManager by inject(JwtManager::class.java)
         val userId = jwtManager.getVerifier().verify(token).getClaim("userId").asLong()
