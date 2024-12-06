59f1669adff6e2d001bdefe0d6c0cdb179ba08f1
Erick Cestari <erickcestari03@gmail.com>
2024-12-05 22:13:32 -0300

    fix test

--- src/features/authentication/routes/signInUser/SignInUser.kt
+++ src/features/authentication/routes/signInUser/SignInUser.kt
@@ -8,12 +8,21 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
 import org.koin.java.KoinJavaComponent.inject
 
 fun Route.signInUser() {
     val authenticationData: AuthenticationData by inject(AuthenticationData::class.java)
     post("/public-api/v1/authentication/signup") {
         val request = call.receive<UserInfoDto>()
+        val cookieConfig = CookieConfiguration().apply {
+            path = "/"
+            secure = true
+            httpOnly = true
+            maxAgeInSeconds = 3600
+            extensions["SameSite"] = "none"
+        }
+
         val token = authenticationData.signIn(request)
         val cookie = Cookie(
             name = "token",
@@ -22,7 +31,7 @@ fun Route.signInUser() {
             path = "/",
             secure = true,
             maxAge = 3600,
-            extensions = mapOf("SameSite" to "none")
+            extensions = cookieConfig.extensions
         )
 
         call.response.cookies.append(cookie)
